6fb744e8b46d5457112e932ca983d732922bcf8f
Pengfei Ding <9438483+dingp@users.noreply.github.com>
2024-08-13 23:58:46 -0700

    remove fixed repo name in start_runner.sh

--- scripts/start_runner.sh
+++ scripts/start_runner.sh
@@ -10,9 +10,7 @@ cd ${gh_dir}
 gh release download --repo actions/runner -p "*linux-x64*"
 tar xvf actions-runner-linux-x64*.tar.gz
 
-runner_token=$(gh api --method POST -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" /repos/dingp/acme/actions/runners/registration-token |jq -r '.token')
+runner_token=$(gh api --method POST -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" /repos/${repo}/actions/runners/registration-token |jq -r '.token')
 
 ./config.sh --unattended --url https://github.com/${repo} --token ${runner_token} --ephemeral
 nohup ./run.sh &
-
-
